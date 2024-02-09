44b21786b9585cad0fdb65560e35e0ecad0122ae
Denis Ciglaric <denisciglaric@gmail.com>
2024-02-09 20:47:47 +0100

    Vue.js show invoices with customer name if exists

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
         //\App\Models\Counter::factory(1)->create();
         //\App\Models\Product::factory(1)->create();
         //\App\Models\Customer::factory(1)->create();
-        //\App\Models\Invoice::factory(1)->create();
-        \App\Models\InvoiceItem::factory(1)->create();
+        //\App\Models\Invoice::factory(5)->create();
+        //\App\Models\InvoiceItem::factory(1)->create();
 
     }
 }
